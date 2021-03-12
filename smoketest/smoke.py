d7890e0bd2b5ebe60b7eafc24d1ce145e6a6c36b
Mark Tsuchida <matsuchida@wisc.edu>
2021-03-12 16:38:59 -0600

    Include device interface version in version number
    The pymmcore version number now includes the device interface version.
    
    Note that MMCore went from device interface version 69 to 70 while
    keeping the MMCore version 10.1.1. This is relatively unusual, but
    including the device interface version in the pymmcore version helps
    working with this situation and is also probably generally helpful.

--- smoketest/smoke.py
+++ smoketest/smoke.py
@@ -5,7 +5,7 @@ import os.path
 
 
 #
-# At the moment, the only test is that our version number is correct
+# At the moment, the only test is that our version numbering is correct
 #
 
 
@@ -18,10 +18,24 @@ config.read(setup_cfg)
 pymmcore_version = config['metadata']['version'].split('.')
 print("Version: {}".format(pymmcore_version))
 
-# getVersionInfo() returns a string like "MMCore version x.y.z"
-mmcore_version = pymmcore.CMMCore().getVersionInfo().split()[-1].split('.')
+mmc = pymmcore.CMMCore()
+
+# getVersionInfo() returns a string like "MMCore version 10.1.1"
+mmcore_version = mmc.getVersionInfo().split()[-1].split('.')
 print("MMCore version: {}".format(mmcore_version))
 
 for i in range(3):
     if pymmcore_version[i] != mmcore_version[i]:
         raise AssertionError('Version mismatch between pymmcore and MMCore')
+
+
+# getAPIVersionInfo() returns a string like
+# "Device API version 69, Module API version 10"
+dev_if_version = mmc.getAPIVersionInfo().split(',')[0].split()[-1]
+mod_if_version = mmc.getAPIVersionInfo().split()[-1]
+print("MMDevice device interface version: {}".format(dev_if_version))
+print("MMDevice module interface version: {}".format(mod_if_version))
+
+if pymmcore_version[3] != dev_if_version:
+    raise AssertionError(
+            'Version mismatch between pymmcore and device interface')
